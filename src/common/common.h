9e4b331cc3431fa18eb69b254dc49e377fd75fa2
MatthieuGomes <141504473+MatthieuGomes@users.noreply.github.com>
2024-12-12 12:45:01 +0100

    Question 1
    * correction compile script + basic setup
    
    * ajout d'un fichier base.h pour gerer les includes communs aux 2 programes
    
    * lecture des paramètres d'entrés pour les 2 programmes
    
    * renommage de base en common
    
    * fix d'une coquille dans gettftp
    
    * report

---
+++ src/common/common.h
@@ -0,0 +1,11 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX_ARGUMENT 4
+#define MIN_ARGUMENT 3
+#define TRUE 1
+#define FALSE 0
