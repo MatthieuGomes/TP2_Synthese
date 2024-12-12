6a168c29e882f0516f3ad045375bf6cfef30a93f
matthieuGomes <matthieu.gomes@ensea.fr>
2024-12-12 12:27:45 +0100

    lecture des paramètres d'entrés pour les 2 programmes

---
+++ src/puttftp/puttftp.c
@@ -0,0 +1,18 @@
+#include "puttftp.h"
+#include "functions.h"
+
+int main(int argc, char *argv[]) { 
+    char * host_address = argv[1];
+    char * host_port = argv[2];
+    char * file = argv[3];
+
+    if (argc < MIN_ARGUMENT || argc > MAX_ARGUMENT) {
+        fprintf(stderr,"Wrong number of arguments for %s", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+    printf("host_address: %s\n", host_address);
+    printf("host_port: %s\n", host_port);
+    printf("file: %s\n", file);
+
+    return EXIT_SUCCESS;
+}
---
+++ src/puttftp/puttftp.h
@@ -0,0 +1,3 @@
+#include"../base/base.h"
+
+int main(int argc, char *argv[]);
