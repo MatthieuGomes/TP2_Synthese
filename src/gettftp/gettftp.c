c80f88349abd2f8f71b00ea85667d11d103330cc
matthieuGomes <matthieu.gomes@ensea.fr>
2024-12-12 12:40:40 +0100

    fix d'une coquille dans gettftp

--- src/gettftp/gettftp.c
+++ src/gettftp/gettftp.c
@@ -10,7 +10,6 @@ int main(int argc, char *argv[]) {
         fprintf(stderr,"Wrong number of arguments for %s", argv[0]);
         exit(EXIT_FAILURE);
     }
-    printf("file: %s\n", file);
     printf("host_address: %s\n", host_address);
     printf("host_port: %s\n", host_port);
     printf("file: %s\n", file);
